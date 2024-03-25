89754cc21973ff2c29e7b4095f1655668696622c
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-03-25 18:24:38 +0100

    ejercicio 8

---
+++ http-collect.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const bl = require('bl')
+const url = process.argv[2]
+
+http.get(url, (res) => {
+    res.pipe(bl((err, data) => {
+        console.log(data.length)
+        console.log(data.toString())
+    }))
+})
